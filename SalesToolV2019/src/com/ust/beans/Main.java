a57723ed0ae8fb7d694cafd8dc4c916d6958a35b
user48 <user48@user48-System-Product-Name>
2019-09-24 14:53:37 +0530

    initial commit by dinkuu

---
+++ SalesToolV2019/src/com/ust/beans/Main.java
@@ -0,0 +1,19 @@
+package com.ust.beans;
+
+public class Main {
+	public static void main(String[] args){
+		SalesData objSalesData=new SalesData();
+		System.out.println("Welcome to sales app");
+		displayGreeting();
+		objSalesData.display();
+		
+		
+	}
+
+	private static void displayGreeting() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
